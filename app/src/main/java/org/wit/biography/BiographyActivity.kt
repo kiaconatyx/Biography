0c8e508f793f14ae4255749dd3a1922356292757
Kia Conaty <20088426@mail.wit.ie>
2023-03-01 16:07:32 +0000

    Initialise Java Timber

--- app/src/main/java/org/wit/biography/BiographyActivity.kt
+++ app/src/main/java/org/wit/biography/BiographyActivity.kt
@@ -2,10 +2,14 @@ package org.wit.biography
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class BiographyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_biography)
+        Timber.plant(Timber.DebugTree())
+        Timber.i("Biography Activity started..")
+
     }
 }
