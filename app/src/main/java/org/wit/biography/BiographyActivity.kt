20e51ac4aa8d53ed192689a3858d464e82b6911b
Kia Conaty <20088426@mail.wit.ie>
2023-03-01 16:25:09 +0000

    Add Biography String

--- app/src/main/java/org/wit/biography/BiographyActivity.kt
+++ app/src/main/java/org/wit/biography/BiographyActivity.kt
@@ -3,13 +3,14 @@ package org.wit.biography
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import timber.log.Timber
+import timber.log.Timber.i
 
 class BiographyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_biography)
         Timber.plant(Timber.DebugTree())
-        Timber.i("Biography Activity started..")
+        i("Biography Activity started..")
 
     }
 }
