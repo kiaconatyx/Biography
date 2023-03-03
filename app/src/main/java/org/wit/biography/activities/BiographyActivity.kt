2fc80e74cf788f8758573a9a685149f3521678c6
Kia Conaty <20088426@mail.wit.ie>
2023-03-03 15:57:10 +0000

    Update BiographyActivity.kt

--- app/src/main/java/org/wit/biography/activities/BiographyActivity.kt
+++ app/src/main/java/org/wit/biography/activities/BiographyActivity.kt
@@ -13,51 +13,49 @@ import timber.log.Timber
 import timber.log.Timber.i
 
 class BiographyActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityBiographyBinding
     var biography = BiographyModel()
-    //val biographys = ArrayList<BiographyModel>()
-lateinit var app : MainApp
+    lateinit var app: MainApp
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        var edit = false
         binding = ActivityBiographyBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
         binding.toolbarAdd.title = title
         setSupportActionBar(binding.toolbarAdd)
-
-
         app = application as MainApp
+
         if (intent.hasExtra("biography_edit")) {
+            edit = true
             biography = intent.extras?.getParcelable("biography_edit")!!
             binding.biographyTitle.setText(biography.title)
             binding.biographyDescription.setText(biography.description)
-            binding.biographyAuthor.setText(biography.author)
             binding.biographyISBN.setText(biography.ISBN)
+            binding.biographyAuthor.setText(biography.author)
             binding.btnAdd.setText(R.string.save_biography)
-
         }
 
         binding.btnAdd.setOnClickListener() {
             biography.title = binding.biographyTitle.text.toString()
             biography.description = binding.biographyDescription.text.toString()
-            biography.ISBN = binding.biographyISBN.text.toString()
             biography.author = binding.biographyAuthor.text.toString()
-            if (biography.title.isNotEmpty()) {
-                app.biographys.create(biography.copy())
-                setResult(RESULT_OK)
-                finish()
-            }
-            else {
-                Snackbar
-                    .make(it,R.string.enter_biography_title, Snackbar.LENGTH_LONG)
+            biography.ISBN = binding.biographyISBN.text.toString()
+            if (biography.title.isEmpty()) {
+                Snackbar.make(it,R.string.enter_biography_title, Snackbar.LENGTH_LONG)
                     .show()
+            } else {
+                if (edit) {
+                    app.biographys.update(biography.copy())
+                } else {
+                    app.biographys.create(biography.copy())
+                }
             }
+            setResult(RESULT_OK)
+            finish()
         }
-
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_biography, menu)
         return super.onCreateOptionsMenu(menu)
