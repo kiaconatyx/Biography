75f5d06330258c3423b864e3e680877978ff0acc
Kia Conaty <20088426@mail.wit.ie>
2023-03-03 14:22:48 +0000

    Update BiographyAdapter.kt

--- app/src/main/java/org/wit/biography/adapters/BiographyAdapter.kt
+++ app/src/main/java/org/wit/biography/adapters/BiographyAdapter.kt
@@ -1,4 +1,4 @@
-package org.wit.placemark.adapters
+package org.wit.biography.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -6,18 +6,24 @@ import androidx.recyclerview.widget.RecyclerView
 import org.wit.biography.databinding.CardBiographyBinding
 import org.wit.biography.models.BiographyModel
 
-class BiographyAdapter constructor(private var biographys: List<BiographyModel>) :
+
+interface BiographyListener {
+    fun onBiographyClick(biography: BiographyModel)
+}
+class BiographyAdapter constructor(private var biographys: List<BiographyModel>,
+                                   private val listener: BiographyListener) :
     RecyclerView.Adapter<BiographyAdapter.MainHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
         val binding = CardBiographyBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
+
         return MainHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MainHolder, position: Int) {
         val biography = biographys[holder.adapterPosition]
-        holder.bind(biography)
+        holder.bind(biography, listener)
     }
 
     override fun getItemCount(): Int = biographys.size
@@ -25,12 +31,12 @@ class BiographyAdapter constructor(private var biographys: List<BiographyModel>)
     class MainHolder(private val binding : CardBiographyBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(biography: BiographyModel) {
-            binding.biographyTitle.text = biography.title
+        fun bind(biography: BiographyModel, listener: BiographyListener) {
+            binding. biographyTitle.text = biography.title
             binding.biographyDescription.text = biography.description
             binding.biographyISBN.text = biography.ISBN
             binding.biographyAuthor.text = biography.author
-
+            binding.root.setOnClickListener { listener.onBiographyClick(biography) }
         }
     }
 }
