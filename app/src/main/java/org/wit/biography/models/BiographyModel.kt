92a05628fc9a5101f7e5fc075b749c245f53cd3d
Kia Conaty <20088426@mail.wit.ie>
2023-03-03 14:37:40 +0000

    Passing Data

--- app/src/main/java/org/wit/biography/models/BiographyModel.kt
+++ app/src/main/java/org/wit/biography/models/BiographyModel.kt
@@ -1,7 +1,11 @@
 package org.wit.biography.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class BiographyModel(var title: String ="",
                         var description: String="",
                           var ISBN: String="",
-                          var author: String=""
-)
+                          var author: String="") : Parcelable
+
