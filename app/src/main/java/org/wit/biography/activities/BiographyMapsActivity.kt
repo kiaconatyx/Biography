b5fdf803a85dd1e3ea7930902dd7863db615c612
Kia Conaty <20088426@mail.wit.ie>
2023-04-27 18:34:08 +0100

    Simplify BiographyMapsActivity

--- app/src/main/java/org/wit/biography/activities/BiographyMapsActivity.kt
+++ app/src/main/java/org/wit/biography/activities/BiographyMapsActivity.kt
@@ -3,40 +3,22 @@ package org.wit.biography.activities
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import org.wit.biography.R
-//import org.wit.biography.activities.databinding.ActivityBiographyMapsBinding
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.navigation.ui.AppBarConfiguration
 import org.wit.biography.databinding.ActivityBiographyMapsBinding
-
 class BiographyMapsActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-private lateinit var binding: ActivityBiographyMapsBinding
+    private lateinit var binding: ActivityBiographyMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-     binding = ActivityBiographyMapsBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+        binding = ActivityBiographyMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_biography_maps)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-    val navController = findNavController(R.id.nav_host_fragment_content_biography_maps)
-    return navController.navigateUp(appBarConfiguration)
-            || super.onSupportNavigateUp()
-    }
 }
