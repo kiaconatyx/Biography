a689759a2975793f2f1c8e2270f6d29efd29f0c0
Kia Conaty <20088426@mail.wit.ie>
2023-04-27 16:01:53 +0100

    Listening For Marker Drag Events

--- app/src/main/java/org/wit/biography/activities/MapActivity.kt
+++ app/src/main/java/org/wit/biography/activities/MapActivity.kt
@@ -1,5 +1,7 @@
 package org.wit.biography.activities
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,13 +10,14 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 //import org.wit.biography.activities.databinding.ActivityMapBinding
 import org.wit.biography.R
 import org.wit.biography.databinding.ActivityMapBinding
 import org.wit.biography.models.Location
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback,  GoogleMap.OnMarkerDragListener {
 
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapBinding
@@ -38,6 +41,30 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             .draggable(true)
             .position(loc)
         map.addMarker(options)
+        map.setOnMarkerDragListener(this)
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, location.zoom))
+
     }
-}
+
+        override fun onMarkerDrag(p0: Marker) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onMarkerDragEnd(marker: Marker) {
+            location.lat = marker.position.latitude
+            location.lng = marker.position.longitude
+            location.zoom = map.cameraPosition.zoom
+        }
+
+        override fun onMarkerDragStart(p0: Marker) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBackPressed() {
+            val resultIntent = Intent()
+            resultIntent.putExtra("location", location)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+            super.onBackPressed()
+        }
+    }
