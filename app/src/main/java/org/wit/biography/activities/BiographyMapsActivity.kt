5587f1ebd1980615a630ca25a97b3867c7ba97d2
Kia Conaty <20088426@mail.wit.ie>
2023-04-27 18:43:33 +0100

    Content Layouts

--- app/src/main/java/org/wit/biography/activities/BiographyMapsActivity.kt
+++ app/src/main/java/org/wit/biography/activities/BiographyMapsActivity.kt
@@ -1,24 +1,54 @@
 package org.wit.biography.activities
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.gms.maps.GoogleMap
+
 import org.wit.biography.databinding.ActivityBiographyMapsBinding
+import org.wit.biography.databinding.ContentBiographyMapsBinding
+
 class BiographyMapsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBiographyMapsBinding
+    private lateinit var contentBinding: ContentBiographyMapsBinding
+    lateinit var map: GoogleMap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityBiographyMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
+        contentBinding = ContentBiographyMapsBinding.bind(binding.root)
+        contentBinding.mapView.onCreate(savedInstanceState)
 
     }
 
+
+    override fun onDestroy() {
+        super.onDestroy()
+        contentBinding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        contentBinding.mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        contentBinding.mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        contentBinding.mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        contentBinding.mapView.onSaveInstanceState(outState)
+    }
 }
