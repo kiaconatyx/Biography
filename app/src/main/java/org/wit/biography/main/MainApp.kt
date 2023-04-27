110da88e97716ee29394724b256e6da2da9256b4
Kia Conaty <20088426@mail.wit.ie>
2023-04-27 17:09:03 +0100

    MainApp Adjustments

--- app/src/main/java/org/wit/biography/main/MainApp.kt
+++ app/src/main/java/org/wit/biography/main/MainApp.kt
@@ -4,19 +4,18 @@ package org.wit.biography.main
 import android.app.Application
 import org.wit.biography.models.BiographyMemStore
 import org.wit.biography.models.BiographyModel
+import org.wit.biography.models.BiographyStore
 import timber.log.Timber
 import timber.log.Timber.i
 
 class MainApp : Application() {
 
-    //val biographys = ArrayList<BiographyModel>()
-    val biographys = BiographyMemStore()
+    lateinit var biographys: BiographyStore
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        biographys = BiographyMemStore()
         i("Biography started")
-       // biographys.add(BiographyModel("One", "About one...", "Unique Code one", "Creator of One"))
-        //biographys.add(BiographyModel("Two", "About two...", "Unique Code two", "Creator of Two"))
-       // biographys.add(BiographyModel("Three", "About three...", "Unique Code three", "Creator of Three"))
     }
 }
