af66cb87cb230c19ffb5ba0a7bada0b559475c2d
Kia Conaty <20088426@mail.wit.ie>
2023-04-27 18:46:54 +0100

    Markers

--- app/src/main/java/org/wit/biography/activities/BiographyMapsActivity.kt
+++ app/src/main/java/org/wit/biography/activities/BiographyMapsActivity.kt
@@ -2,20 +2,37 @@ package org.wit.biography.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 import org.wit.biography.databinding.ActivityBiographyMapsBinding
 import org.wit.biography.databinding.ContentBiographyMapsBinding
+import org.wit.biography.main.MainApp
+
 
 class BiographyMapsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBiographyMapsBinding
     private lateinit var contentBinding: ContentBiographyMapsBinding
     lateinit var map: GoogleMap
+    lateinit var app: MainApp
+
+    private fun configureMap() {
+        map.uiSettings.isZoomControlsEnabled = true
+        app.biographys.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.title).position(loc)
+            map.addMarker(options)?.tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        app = application as MainApp
 
         binding = ActivityBiographyMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -24,6 +41,10 @@ class BiographyMapsActivity : AppCompatActivity() {
         contentBinding = ContentBiographyMapsBinding.bind(binding.root)
         contentBinding.mapView.onCreate(savedInstanceState)
 
+        contentBinding.mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
     }
 
 
